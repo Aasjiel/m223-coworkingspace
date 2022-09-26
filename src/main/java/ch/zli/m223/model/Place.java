0412640241de28e5718f2214599aed2e239a1600
Aasjiel <skullmage.pvp@gmail.com>
2022-09-26 11:26:22 +0200

    added model for Place

---
+++ src/main/java/ch/zli/m223/model/Place.java
@@ -0,0 +1,49 @@
+package ch.zli.m223.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+
+@Entity
+public class Place {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Schema(readOnly = true)
+    private Long id;
+
+    @Column
+    private String location;
+
+    @Column
+    private int placeNumber;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public int getPlaceNumber() {
+        return placeNumber;
+    }
+
+    public void setPlaceNumber(int placeNumber) {
+        this.placeNumber = placeNumber;
+    }
+
+    
+}

166cb92c784cc140294024463ab232ad6c99280d
Aasjiel <skullmage.pvp@gmail.com>
2022-09-28 10:21:08 +0200

    added Autorisation check for Update and GetOne and 2 responses

--- src/main/java/ch/zli/m223/controller/BookingController.java
+++ src/main/java/ch/zli/m223/controller/BookingController.java
@@ -2,8 +2,8 @@ package ch.zli.m223.controller;
 
 import java.util.List;
 
+import javax.annotation.security.RolesAllowed;
 import javax.inject.Inject;
-import javax.ws.rs.BadRequestException;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
@@ -11,8 +11,13 @@ import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.SecurityContext;
+import javax.ws.rs.core.Response.Status;
 
+import org.eclipse.microprofile.jwt.JsonWebToken;
 import org.eclipse.microprofile.openapi.annotations.Operation;
 
 import ch.zli.m223.service.BookingService;
@@ -23,7 +28,11 @@ public class BookingController {
     @Inject
     BookingService bookingService;
 
+    @Inject
+    JsonWebToken jwt;
+
     @GET
+    @RolesAllowed({"Admin", "Mitglied"})
     @Produces(MediaType.APPLICATION_JSON)
     @Operation(summary = "Index all Bookings.", description = "Returns a list of all Bookings.")
     public List<Booking> getAllBookings() {
@@ -31,17 +40,23 @@ public class BookingController {
     }
 
     @GET
+    @RolesAllowed({"Admin", "Mitglied"})
     @Produces(MediaType.APPLICATION_JSON)
     @Operation(summary = "Index one Bookings", description = "Returns a Bookings.")
     @Path("/{id}")
-    public Booking index(Long id) {   
+    public Response index(Long id) {   
+            Booking booking = bookingService.getBookingById(id);
             if(bookingService.getBookingById(id) != null) {
-                return bookingService.getBookingById(id);
+                if (jwt.getClaim("id").toString().equals(booking.getUser().getId().toString()) || jwt.getGroups().contains("Admin")) {
+                    return Response.ok(booking).build();
+                }
+                return Response.status(Status.UNAUTHORIZED).entity("This Booking is not one of yours").build();
             } 
-            throw new BadRequestException();
+            return Response.status(Status.UNAUTHORIZED).build();
     }
 
     @POST
+    @RolesAllowed({"Admin","Mitglied"})
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
     @Operation(summary = "Creates a new Bookings.", description = "Creates a new Bookings and returns the newly added entry.")
@@ -50,6 +65,7 @@ public class BookingController {
     }
 
     @DELETE
+    @RolesAllowed("Admin")
     @Operation(
         summary = "Deletes a Bookings",
         description = "Deletes a specified Bookings and returns not a single thing"
@@ -60,13 +76,20 @@ public class BookingController {
     }
 
     @PUT
+    @RolesAllowed({"Admin","Mitglied"})
     @Operation(
         summary = "Updates a Booking",
         description = "Updates a specified Booking and returns not a single thing"
     )
     @Path("/{id}")
-    public void update(Long id, Booking booking){
-        booking.setId(id);
-        bookingService.updateBooking(booking);
+    public Response update(@Context SecurityContext ctx, Long id, Booking booking){
+        if (jwt.getClaim("id").toString().equals(booking.getUser().getId().toString()) || jwt.getGroups().contains("Admin")) {
+            booking.setId(id);
+            bookingService.updateBooking(booking);
+            return Response.ok(booking).build();
+        }
+
+        return Response.status(Status.UNAUTHORIZED).entity("you are not allowed to update this booking").build();
+        
     }
 }
